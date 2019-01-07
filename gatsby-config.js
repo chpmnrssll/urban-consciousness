bd366dd1050d4a0c4ba8d491f61132ca060db43c
Russell Chapman <chpmnrssll@gmail.com>
2019-01-06 17:01:05 -0700

    Update GA tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-36647530-2`,
       },
     },
     `gatsby-plugin-feed`,
